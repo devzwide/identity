5c3de8439950e20aee89b2bb9a90238872e51193
devzwide <nxumalobukeka66@gmail.com>
2026-06-29 17:31:56 +0200

    feat: identity to secure web api: Installed Microsoft.AspNetCore.Identity.EntityFrameworkCore package to enable identity to work with EF Core Installed Microsoft.EntityFrameworkCore.SqlServer package to enable EF Core to work with a database Initiated and configured Identity DB Context class

--- authentication/Program.cs
+++ authentication/Program.cs
@@ -1,6 +1,11 @@
+using authentication.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
